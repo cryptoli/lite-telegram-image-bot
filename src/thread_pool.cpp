999c7903abf47bc6159e9b2b7ddf5ef7b6034dee
cryptoli <812143550@qq.com>
2024-09-01 19:58:31 +0800

    修复部分bug

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -41,22 +41,3 @@ void ThreadPool::resize(size_t newSize) {
         workers.pop_back();
     }
 }
-
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queueMutex);
-
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-
-        tasks.emplace([task](){ (*task)(); });
-    }
-    condition.notify_one();
-    return res;
-}
