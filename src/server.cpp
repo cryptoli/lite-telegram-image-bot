a97df27c238d4de58c4025e818bbb423bf49f2ec
cryptoli <812143550@qq.com>
2024-09-02 04:22:43 +0800

    注册不同类型文件路由

--- src/server.cpp
+++ src/server.cpp
@@ -13,10 +13,32 @@ void startServer(const Config& config, ImageCacheManager& cacheManager, ThreadPo
     // 使用 SSL 证书和密钥初始化 HTTPS 服务器
     httplib::SSLServer svr("server.crt", "server.key");
 
+    // 注册不同类型文件的路由
     svr.Get(R"(/images/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
         handleImageRequest(req, res, apiToken, mimeTypes, cacheManager);
     });
 
+    svr.Get(R"(/files/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleFileRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    svr.Get(R"(/videos/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleVideoRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    svr.Get(R"(/audios/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleAudioRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    svr.Get(R"(/gifs/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleGifRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    svr.Get(R"(/stickers/([^\s/]+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleStickerRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    // 启动服务器
     std::future<void> serverFuture = pool.enqueue([&svr, hostname, port]() {
         log(LogLevel::INFO,"Server thread running on port: " + std::to_string(port));
         if (!svr.listen(hostname.c_str(), port)) {
