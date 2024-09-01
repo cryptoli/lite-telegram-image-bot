89469a76e87fc097096708ef4b58e0690149f2a6
cryptoli <812143550@qq.com>
2024-09-01 23:27:26 +0800

    修改server

--- src/server.cpp
+++ src/server.cpp
@@ -1,11 +1,25 @@
-#ifndef SERVER_H
-#define SERVER_H
+#include "server.h"
+#include "request_handler.h"
+#include "logger.h"
 
-#include "httplib.h"
-#include "config.h"
-#include "image_cache_manager.h"
-#include "thread_pool.h"
+void startServer(const Config& config, ImageCacheManager& cacheManager, ThreadPool& pool) {
+    std::string apiToken = config.getApiToken();
+    std::string hostname = config.getHostname();
+    int port = config.getPort();
+    auto mimeTypes = config.getMimeTypes();
 
-void startServer(const Config& config, ImageCacheManager& cacheManager, ThreadPool& pool);
+    httplib::Server svr;
 
-#endif
+    svr.Get(R"(/images/(\w+))", [&apiToken, &mimeTypes, &cacheManager](const httplib::Request& req, httplib::Response& res) {
+        handleImageRequest(req, res, apiToken, mimeTypes, cacheManager);
+    });
+
+    std::future<void> serverFuture = pool.enqueue([&svr, hostname, port]() {
+        log("Server thread running on port: " + std::to_string(port));
+        if (!svr.listen(hostname.c_str(), port)) {
+            log("Error: Server failed to start on port: " + std::to_string(port));
+        }
+    });
+
+    serverFuture.get();
+}
