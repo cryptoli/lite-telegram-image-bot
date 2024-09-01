8cc7637e410fd0fc268d2d4f58db459cd41b6218
cryptoli <812143550@qq.com>
2024-09-02 00:24:44 +0800

    修复日志bug

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -26,7 +26,7 @@ std::string sendHttpRequest(const std::string& url) {
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
         res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
-            log(LogLevel::ERROR,"curl_easy_perform() failed: " +res );
+            log(LogLevel::ERROR,"curl_easy_perform() failed!");
         }
         curl_easy_cleanup(curl);
     } else {
