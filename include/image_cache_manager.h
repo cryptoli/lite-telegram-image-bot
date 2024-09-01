6a012af927cf2ee7937558d4458bc6d651a465d4
cryptoli <812143550@qq.com>
2024-09-01 22:55:03 +0800

    修改缓存代码

--- include/image_cache_manager.h
+++ include/image_cache_manager.h
@@ -4,21 +4,21 @@
 #include <string>
 #include <unordered_map>
 #include <chrono>
-#include <boost/filesystem.hpp>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 #include <fstream>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <algorithm>
 
-namespace fs = boost::filesystem;
-
 class ImageCacheManager {
 public:
     ImageCacheManager(const std::string& cacheDir, size_t maxDiskUsageMB, int maxCacheAgeSeconds)
         : cacheDir(cacheDir), maxDiskUsageBytes(maxDiskUsageMB * 1024 * 1024), maxCacheAgeSeconds(maxCacheAgeSeconds) {
-        if (!fs::exists(cacheDir)) {
-            fs::create_directories(cacheDir);
+        if (!directoryExists(cacheDir)) {
+            mkdir(cacheDir.c_str(), 0777);
         }
         cleanerThread = std::thread(&ImageCacheManager::cleanUpOldFiles, this);
     }
@@ -47,7 +47,7 @@ public:
         std::lock_guard<std::mutex> lock(cacheMutex);
         std::string filePath = getCacheFilePath(fileId);
 
-        if (fs::exists(filePath)) {
+        if (fileExists(filePath)) {
             lastAccessTimes[fileId] = std::chrono::system_clock::now();
 
             std::ifstream file(filePath.c_str(), std::ios::binary);
@@ -73,17 +73,38 @@ private:
 
     size_t getCacheSize() const {
         size_t totalSize = 0;
-        for (fs::directory_iterator itr(cacheDir); itr != fs::directory_iterator(); ++itr) {
-            if (fs::is_regular_file(itr->status())) {
-                totalSize += fs::file_size(itr->path());
+        DIR* dirp = opendir(cacheDir.c_str());
+        struct dirent* entry;
+        while ((entry = readdir(dirp)) != nullptr) {
+            std::string filePath = cacheDir + "/" + entry->d_name;
+            struct stat filestat;
+            if (stat(filePath.c_str(), &filestat) == 0 && S_ISREG(filestat.st_mode)) {
+                totalSize += filestat.st_size;
             }
         }
+        closedir(dirp);
         return totalSize;
+    }
+
+    bool directoryExists(const std::string& path) {
+        struct stat info;
+        if (stat(path.c_str(), &info) != 0) {
+            return false;
+        } else if (info.st_mode & S_IFDIR) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool fileExists(const std::string& path) {
+        struct stat buffer;
+        return (stat(path.c_str(), &buffer) == 0);
     }
 
     void cleanUpOldFiles() {
         while (!stopCleaner) {
-            std::this_thread::sleep_for(std::chrono::seconds(60)); // 每60秒检查一次
+            std::this_thread::sleep_for(std::chrono::seconds(600)); // 每600秒检查一次
 
             std::lock_guard<std::mutex> lock(cacheMutex);
 
@@ -93,12 +114,12 @@ private:
             for (auto it = lastAccessTimes.begin(); it != lastAccessTimes.end();) {
                 std::string filePath = getCacheFilePath(it->first);
 
-                if (fs::exists(filePath)) {
+                if (fileExists(filePath)) {
                     auto fileAge = std::chrono::duration_cast<std::chrono::seconds>(now - it->second).count();
 
                     // 删除长时间未访问的文件
                     if (fileAge > maxCacheAgeSeconds) {
-                        fs::remove(filePath);
+                        remove(filePath.c_str());
                         it = lastAccessTimes.erase(it);
                         std::cout << "Removed old cached image: " << filePath << std::endl;
                     } else {
@@ -119,9 +140,9 @@ private:
 
                 if (oldest != lastAccessTimes.end()) {
                     std::string oldestFilePath = getCacheFilePath(oldest->first);
-                    if (fs::exists(oldestFilePath)) {
-                        currentCacheSize -= fs::file_size(oldestFilePath);
-                        fs::remove(oldestFilePath);
+                    if (fileExists(oldestFilePath)) {
+                        currentCacheSize -= getFileSize(oldestFilePath);
+                        remove(oldestFilePath.c_str());
                         lastAccessTimes.erase(oldest);
                         std::cout << "Removed image due to disk space limit: " << oldestFilePath << std::endl;
                     }
@@ -131,6 +152,14 @@ private:
             }
         }
     }
+
+    size_t getFileSize(const std::string& path) {
+        struct stat filestat;
+        if (stat(path.c_str(), &filestat) == 0) {
+            return filestat.st_size;
+        }
+        return 0;
+    }
 };
 
-#endif 
+#endif
